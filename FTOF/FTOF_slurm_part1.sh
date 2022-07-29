6b4b5fa6164a26d92c2dfe4d8e7d7f075278ab54
matthewnicol15 <56081683+matthewnicol15@users.noreply.github.com>
2022-07-29 11:02:38 +0100

    Update FTOF_slurm_part1.sh
    Made option 1 run between two variables defined in the shell script so both options run in a very similar way

--- FTOF/FTOF_slurm_part1.sh
+++ FTOF/FTOF_slurm_part1.sh
@@ -5,7 +5,7 @@
 ############################################################################
 
 # jcache hipo files that are not in the cache already
-# Make sure to load clas12root: 
+# Make sure to load clas12root:
 ### source /group/clas12/packages/setup.csh
 ### module load clas12/pro
 # Make sure you have access to slurm account clas12
@@ -33,20 +33,18 @@ DataDir="/cache/clas12/rg-a/production/recon/fall2018/torus-1/pass1/v0/dst/recon
 ## Option 1 ##
 ## You can either run over all available runs between two numbers with option 1
 ## For example "5032 5038" would run over runs: 5032, 5036, and 5038
-
+Min=5032
+Max=5038
 
 ## Option 2 ##
 ## Or you can specify a list of runs you would like to analyse.
-## Simply list them inside the brackets below separated by a space 
+## Simply list them inside the brackets below separated by a space
 
 RUNS=(5032 5036 5038)
 
-# Make sure the option you want is included in section Making RCDB Files and the one 
+# Make sure the option you want is included in section Making RCDB Files and the one
 # you don't is commented out
-# To run option 1 
-# ./FTOF_slurm_part1.sh minimumrun maximumrun
-# To run option 2
-# ./FTOF_slurm_part1.sh
+
 
 #############
 # TOF variables
@@ -98,7 +96,7 @@ touch $List
 ############################################################################
 
 ## Option 1 ##
-for NUMBER in `seq $1 $2`;
+for NUMBER in $(seq $Min $Max);
 
 ## Option 2 ##
 #for NUMBER in "${RUNS[@]}"
@@ -111,8 +109,8 @@ do
    if [ ! -d "$FILE" ]; then
       continue
    fi
-   
-   
+
+
    # Check if the RCDB slurm files exists
    RCDBSlurm='process_PrepareDatabases_'$NUMBER'.sh'
    if [ -f "$RCDBSlurm" ]; then
@@ -120,20 +118,20 @@ do
       rm slurm_PrepareDatabases_"$NUMBER".sh
       echo removed
    fi
-   
-   
-   
+
+
+
    touch process_PrepareDatabases_"$NUMBER".sh
-   
+
    echo 'gROOT->ProcessLine(".L '$MacroLoc'/PrepareDatabases.C");' >> process_PrepareDatabases_"$NUMBER".sh
    echo 'gROOT->ProcessLine("PrepareDatabases(\"'$DataDir'/00'$NUMBER'/*.hipo\",\"'$OutputDir'/RCDB/'$RCDBOutputName'_'$NUMBER'.root\")");' >> process_PrepareDatabases_"$NUMBER".sh
-   
+
    echo "Made process_PrepareDatabases_"$NUMBER".sh"
-   
-   
-   
+
+
+
    touch slurm_PrepareDatabases_"$NUMBER".sh
-   
+
    echo '#!/bin/bash' >> slurm_PrepareDatabases_"$NUMBER".sh
    echo '#SBATCH --nodes=1' >> slurm_PrepareDatabases_"$NUMBER".sh
    echo '#SBATCH --ntasks=1' >> slurm_PrepareDatabases_"$NUMBER".sh
@@ -142,39 +140,39 @@ do
    echo '#SBATCH --partition=production' >> slurm_PrepareDatabases_"$NUMBER".sh
    echo '#SBATCH --account=clas12' >> slurm_PrepareDatabases_"$NUMBER".sh
    echo '/bin/more process_PrepareDatabases_'$NUMBER'.sh | clas12root -b' >> slurm_PrepareDatabases_"$NUMBER".sh
-   
+
    echo "Made slurm_PrepareDatabases_"$NUMBER".sh"
-   
+
    # Running the RCDB file creating
    RES=$(sbatch slurm_PrepareDatabases_$NUMBER.sh)
-   
-   
+
+
    sleep 2
-   
+
    ############################################################################
    # Making FTOF efficieny files
    ############################################################################
-   
+
    # Check if the FTOF slurm files exists
    FTOFSlurm='process_FTOF'$NUMBER'.sh'
    if [ -f "$FTOFSlurm" ]; then
       rm process_FTOF"$NUMBER".sh
       rm slurm_FTOF_$NUMBER.sh
    fi
-   
-   
+
+
    touch process_FTOF"$NUMBER".sh
-   
-   
+
+
    echo 'gROOT->ProcessLine(".L '$MacroLoc'/FTOF_Efficiency.C");' >> process_FTOF"$NUMBER".sh
    echo 'gROOT->ProcessLine("FTOF_Efficiency(\"'$OutputDir'/RCDB/'$RCDBOutputName'_'$NUMBER'.root\",\"'$DataDir'/00'$NUMBER'/*.hipo\",\"'$OutputDir'/ROOT/'$OutputName$NUMBER'.root\",\"'$Polarity'\",\"'$targetMass'\")");' >> process_FTOF"$NUMBER".sh
-   
+
    echo $OutputDir"/ROOT/"$OutputName$NUMBER".root" >> $List
-   
+
    echo "Made process_FTOF"$NUMBER".sh"
-   
+
    touch slurm_FTOF_$NUMBER.sh
-   
+
    echo '#!/bin/bash' >> slurm_FTOF_$NUMBER.sh
    echo '#SBATCH --nodes=1' >> slurm_FTOF_$NUMBER.sh
    echo '#SBATCH --ntasks=1' >> slurm_FTOF_$NUMBER.sh
@@ -183,10 +181,10 @@ do
    echo '#SBATCH --partition=production' >> slurm_FTOF_$NUMBER.sh
    echo '#SBATCH --account=clas12' >> slurm_FTOF_$NUMBER.sh
    echo '/bin/more process_FTOF'$NUMBER'.sh | clas12root -b' >> slurm_FTOF_$NUMBER.sh
-   
+
    echo "Made /slurm_FTOF_"$NUMBER".sh"
-   
+
    sbatch --dependency=afterok:${RES##* } slurm_FTOF_$NUMBER.sh
    sleep 2
-   
+
 done
